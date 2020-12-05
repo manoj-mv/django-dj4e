ba3a889e76d2a6b090a966d01e8a38091419b5fa
Manoj MV <manojmv3019@gmail.com>
2020-12-05 10:12:00 +0530

    favourite app completed

--- favourite/urls.py
+++ favourite/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('create',views.ThingCreateView.as_view(),name='thing_create'),
     path('update/<int:pk>',views.ThingUpdateView.as_view(),name='thing_update'),
     path('delete/<int:pk>',views.ThingDeleteView.as_view(),name='thing_delete'),
+    path('add-to-favourite/<int:pk>',views.AddFavourite.as_view(),name="add_favourite"),
+    path('unfavourite/<int:pk>',views.UnFavourite.as_view(),name='unfavourite')
 ]
--- favourite/views.py
+++ favourite/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.views import View
 from django.urls import reverse_lazy
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
+from django.db.utils import IntegrityError
 
 from .models import Thing,Fav
 from forum_dj4e.owner import OwnerCreateView,OwnerDeleteView,OwnerListView,OwnerUpdateView
@@ -10,15 +12,28 @@ class ThingListView(OwnerListView):
     template_name = 'favourite/index.html'
 
     def get(self,request):
+        print('Executing ThingListView get()')
+        fav_list=None
         thing_list = Thing.objects.all()
-        print(thing_list)
+        # print(thing_list)
         usr = request.user
         # print(dir(usr))
         # print(fvrt_list)
-        try:
-            fav_list = Fav.objects.all().filter(user=usr)
-        except:
-            fav_list=None
+        if usr.is_authenticated:
+            print('user is authenticated')
+            try:
+                print('Entered try block')
+                fav_l = Fav.objects.all().filter(user=usr)
+                print('fav_l : ',fav_l)
+                fav_list = [fav.thing.id for fav in fav_l]
+               
+                # by referencing many to many field in Thing, below commented code can also be used for access user favourites
+                # l=request.user.favourite_things.values('id') 
+                
+            except:
+                print('Entered catch block in ThingListView get()')
+                fav_list=None
+        print('fav_list:',fav_list)
         ctx={
             'thing_list': thing_list,
             'favourites': fav_list
@@ -38,4 +53,27 @@ class ThingUpdateView(OwnerUpdateView):
 class ThingDeleteView(OwnerDeleteView):
     model = Thing
     success_url = reverse_lazy('fav:index')
+
+class AddFavourite(LoginRequiredMixin, View):
+    def post(self,request,pk):
+        print(pk)
+        print(request.user)
+        thing_obj = get_object_or_404(Thing,pk=pk)
+        fav_obj = Fav(thing=thing_obj,user=request.user)
+        print(fav_obj)
+        try:
+            fav_obj.save() # In case of duplicate key
+        except IntegrityError as e:
+            pass
+        return HttpResponse()
+
+class UnFavourite(LoginRequiredMixin, View):
+    def post(self,request,pk):
+        print(pk)
+        print(request.user)    
+        fav_obj = get_object_or_404(Fav,thing = pk,user=request.user)
+        fav_obj.delete()
+        print('unfavourited :',fav_obj)
+        return HttpResponse()
+
 
